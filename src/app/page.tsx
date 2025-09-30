eef1d2c958bc88ae2c8ad8af2d0fcf8d7a6764db
Duc <thienductranhuu2784@gmail.com>
2025-09-29 23:38:32 -0400

    fetching song data with generic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import AppFooter from "@/components/footer/app.footer";
 import AppHeader from "@/components/header/app.header";
 import MainSlider from "@/components/main/main.slider";
+import { sendRequest } from "@/utils/api";
 import { Category } from "@mui/icons-material";
 import { AppBar, Container, Toolbar } from "@mui/material";
 
-export default function HomePage() {
-  const res = fetch("http://localhost:8000/api/v1/tracks/top", {
+export default async function HomePage() {
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({ //generic in generic
+    url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      category: "WORKOUT",
-      limit: 10
-    })  //because fetch only understands string type, not object type
+    body: { category: "WORKOUT", limit: 1 }
   })
+  console.log("check res server: ", res.data)
   return (
     <Container>
       <MainSlider />
