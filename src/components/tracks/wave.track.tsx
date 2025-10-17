5742d83d7d6ab08982eed7009bcdbda09503452c
Duc <thienductranhuu2784@gmail.com>
2025-10-17 14:47:16 -0400

    got rid of assigning id to every element with useRef

--- src/components/tracks/wave.track.tsx
+++ src/components/tracks/wave.track.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import WaveSurfer from "wavesurfer.js"
+
 const WaveTrack = () => {
+
+    const containerRef = useRef<HTMLDivElement>(null)
 
     //put this wavesufer in useEffect because we only insert this feature into UI after the DOM is complete
     //and everything is ready
     useEffect(() => {
-        if (document.getElementById("hoidanit")) {
+
+        if (containerRef.current) {
             WaveSurfer.create({
-                container: document.getElementById("hoidanit")!,
+                container: containerRef.current,
                 waveColor: 'rgb(200, 0, 200)',
                 progressColor: 'rgb(100, 0, 100)',
                 url: '/audio/hoidanit.mp3',
@@ -17,7 +21,7 @@ const WaveTrack = () => {
         }
     }, [])
     return (
-        <div id="hoidanit">
+        <div ref={containerRef}>
             wave track
         </div>
     )
